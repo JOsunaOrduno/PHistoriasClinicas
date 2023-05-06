b23fc7f017cdf8ce8587c5e9b0a790a121822160
Ladingo2000 <82851676+JOsunaOrduno@users.noreply.github.com>
2023-05-06 01:54:30 -0600

    POST de identificacion terminado
    Ejemplo de post y modificaciones en el index para que solo id funcione

--- App/main.py
+++ App/main.py
@@ -16,7 +16,7 @@ def create_paciente():
         _telefono = _json['telefono']
         _spss = _json['spss']	
         _fechaNac = _json['fechaNac']
-        if _entidadNac and _curp and _sexo and _talla and _domicilio and _telefono and _spss and _fechaNac and request.method == 'POST':
+        if request.method == 'POST':
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)		
             sqlQuery = "INSERT INTO paciente(entNacimiento, curp, sexo, talla, domicilio, telefono, spss, fechaNac) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
@@ -35,6 +35,35 @@ def create_paciente():
         cursor.close() 
         conn.close()          
      
+
+@app.route('/prueba', methods=['POST'])
+def prueba_paciente():
+    try:        
+        _entidadNac = request.form['entidad']
+        _curp = request.form['curp']
+        _sexo = request.form['sexo']
+        _talla = request.form['talla']	
+        _domicilio = request.form['domicilio']
+        _telefono = request.form['telefono']
+        _fechaNac = request.form['fechaNac']
+        if request.method == 'POST':
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)		
+            sqlQuery = "INSERT INTO paciente(entNacimiento, curp, sexo, talla, domicilio, telefono, fechaNac) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+            bindData = (_entidadNac, _curp, _sexo, _talla, _domicilio, _telefono,  _fechaNac)            
+            cursor.execute(sqlQuery, bindData)
+            #cursor.execute("INSERT INTO paciente(entNacimiento, curp, sexo, talla, domicilio, telefono, spss, fechaNac) VALUES(2, 2, 2, 2, 2, 2, 2, 2023-1-1)")
+            conn.commit()
+            respone = jsonify('Employee added successfully!')
+            respone.status_code = 200
+            return respone
+        else:
+            return showMessage()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close()   
 
 
 @app.route('/paciente')
